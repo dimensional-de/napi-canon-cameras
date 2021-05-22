e2531dc517683368540ada433ed52f986029ff05
Thomas Weinert <thomas@weinert.info>
2021-05-22 17:02:32 +0200

    [FEATURE] Add Object Format Enum
    Object format values from Canon support.
    Should be included in the next EDSDK release, define
    in types.h for now.

--- src/library/types.h
+++ src/library/types.h
@@ -14,6 +14,17 @@
 
 #define kEdsImageType_CR3           0x00000008
 
+
+typedef enum {
+    kEdsObjectFormat_Unknown = 0x00000000,
+    kEdsObjectFormat_JPEG = 0x3801,
+    kEdsObjectFormat_CR2 = 0xB103,
+    kEdsObjectFormat_MP4 = 0xB982,
+    kEdsObjectFormat_CR3 = 0xB108,
+    kEdsObjectFormat_HEIF_CODE = 0xB10B
+} EdsObjectFormat;
+
+
 namespace CameraApi {
 
     typedef std::unordered_map<int, std::string> LabelMap;
